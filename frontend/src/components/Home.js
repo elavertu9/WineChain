c80038e7e4ec719d68f4e2523cfb7b0f4afdfee8
elavertu9 <elavertu@rams.colostate.edu>
2020-04-02 11:46:06 -0600

    added wine coins listing mock up and home page image

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Container, Row, Col} from 'reactstrap';
 import "./resources/styles/HomeStyles.css";
+import HomeImage from './resources/images/wine-bottles.jpeg';
 
 export default class Home extends Component {
     constructor(props) {
@@ -12,16 +13,13 @@ export default class Home extends Component {
           <Container>
               <Row>
                   <Col className="element-center">
-                      <h1>Welcome to the WineChain homepage!</h1>
+                      <h1>Welcome to WineChain</h1>
+                      <br/>
+                      <img src={HomeImage} width='85%'className='home-img'/>
                   </Col>
               </Row>
 
               <br/>
-              <Row>
-                  <Col className="element-center">
-                      <button className="button">Example</button>
-                  </Col>
-              </Row>
           </Container>
         );
     }
